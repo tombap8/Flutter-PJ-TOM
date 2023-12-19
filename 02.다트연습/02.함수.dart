37412c8bd93d09bb74a6fa35c01873db2a44659a
Tom B. Parker <tombap8@hotmail.com>
2023-12-19 17:04:46 +0900

    Update 02.함수.dart

--- 02.다트연습/02.함수.dart
+++ 02.다트연습/02.함수.dart
@@ -50,7 +50,7 @@ void main(List<String> args) {
   japanShip((){print('개.박.살...! 일본배 침몰!');});
 
   // 람다함수 연습
-  showTxt('"아직 신에게는 12척의 배가 남았습니다!" 이 대사가 나오는 이순신의 전투는? ${leeFight[0]}');
+  showTxt('"아직 신에게는 12척의 배가 남았습니다!" 이 대사가 나오는 이순신의 전투는? ${retVal(leeFight[0])}');
 
   showTxt("아직 신에게는 ${minus()}척의 배가 남았습니다!");
   showTxt("아직 신에게는 ${minus()}척의 배가 남았습니다!");
@@ -61,10 +61,31 @@ void main(List<String> args) {
 
   showTxt('이순신의 부하중 이순신이있었다. '+
   '그는 전투전에 너무 긴장되어서 구구단을 외웠다! 9단!');
-  // 구구단 출력~!(내일 만나~~~~!)
+  // 구구단 출력~!
+  gugu(9);
+  showTxt('옆사람도 불안하여 7단을 외웠다!');
+  gugu(7);
 
 
 } /////// main ///////////////////////////////////
+
+// 구구단함수 /////
+void gugu (int x) {  
+  // 9단일때 아이콘 다름, 나머진 같음
+  String icon = "";
+
+  if(x==9) {
+    icon = "🤑";
+  }
+  else{
+    icon = "🐶";
+  }
+  print('$icon$x 단');
+  // for문
+  for(int i=1;i<=9;i++){
+    print("$x × $i ＝ ${x*i}");
+  } ////// for /////
+}
 
 
 int shipNum = 13;
@@ -81,10 +102,19 @@ void japanShip(Function bomb){
 
 
 // 추천배우 변수
+// const Map<String,Map> recommActor = {
 const recommActor = {
   '조인성':{'나이':42, '취미':'날기', '사는곳':'아무데나'},
   '공유':{'나이':45, '취미':'비오게하기', '사는곳':'공유하우스'}
 };
+
+// 이순신 전투 뒷말셋팅 함수
+// 결과리턴값의 형을 생각한다!
+String retVal (String x){
+  return "$x${x=='한산'?'도대첩':'해전'}";
+  // 3항연산자 -> 조건문?코드1:코드2
+  // 조건이 true이면 코드1, false이면 코드2
+}
 
 
 // [ 다트의 상수 : final, const ] -> 함수바깥이면 전역변수
