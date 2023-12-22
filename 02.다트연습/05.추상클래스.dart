a3dec7b001252151964bc5090ab016be49271a46
Tom B. Parker <tombap8@hotmail.com>
2023-12-22 11:58:01 +0900

    Update 05.추상클래스.dart

--- 02.다트연습/05.추상클래스.dart
+++ 02.다트연습/05.추상클래스.dart
@@ -53,7 +53,11 @@ abstract class Saram{
 // 도경수 배우의 클래스
 class KyungSu implements Saram {
   @override
-  String get name => '도경수';
+  late String _name; // 셋팅변수
+  String get name => _name;
+  set name(String name) => _name = name;
+
+  // String get name => '도경수';
   // get 키워드는 겟터를 사용하는 변수를 선언함
 
   @override
@@ -63,7 +67,8 @@ class KyungSu implements Saram {
   String age() => '$name씨의 나이는 30살이다!';
 
   @override
-  String eat() => '$name씨는 피자를 먹는다!';
+  String eat(){return '$name씨는 피자를 먹는다!';}
+  // String eat() => '$name씨는 피자를 먹는다!';
 
   @override
   String say() => '$name씨는 무엇인가 말하고 있다!';
@@ -82,3 +87,66 @@ class KyungSu implements Saram {
  * 다르게 여러게 만들수 있는 것으로 말함!
 
 *************************************************/
+
+/*************************************** 
+[ 다트의 겟터 / 셋터 ]
+변수값 읽기와 할당을 하도록 캡슐화를 하여
+값 셋팅시 정보를 은닉하여 변수를 관리하는 방법
+
+((방법:))
+late 변수형 셋팅변수명;
+변수형 get 변수명 => 셋팅변수명;
+set 변수명(변수형 변수명) -> 셋팅변수명 = 변수명
+
+((겟터/셋터 변수명))
+일반적으로 변수명과 셋팅변수명은 같은이름이며 
+셋팅변수명 앞에 언더바(_)를 붙여구분함
+예) 변수명-> name, 셋팅변수명->_name
+
+((late 키워드))
+선언한 변수가 바로 할당되지 못할 경우
+late 키워드를 앞에 사용하여 사용직전에
+할당될 것임일 알려주면 에러를 막을 수 있다!
+
+((겟터/셋터 값할당))
+-> 외부에서 인스턴스 생성후 셋터를 통해 
+값을 셋팅할 경우 변수명에 할당하면 된다!
+클래스.변수명 = 값
+
+ ***************************************/
+
+ /// 남지현 배우 클래스 : Saram 클래스  일반 상속하기
+ /// 일반상속은 부모클래스의 모든 내용을 재정의할 필요없음!
+ /// -> 단, 구체적인 정의가 없는 메서드는 필수재정의!!!
+ class JeeHyun extends Saram {
+  // 생성자 : 받은 값을 부모클래스로 넘겨 먼저셋팅함!
+   JeeHyun(String name) : super(name);
+
+  @override
+  String gender() => '$name씨는 여자다!';
+
+  @override
+  String age() => '$name씨는 28살이다!';
+
+ } ////////// JeeHyun 클래스 ///////////
+
+
+//// 테스트를 수행하는 함수 /////
+void testClass(){
+  // 추상클래스를 구현한 경수클래스 인스턴스생성
+  KyungSu ks = KyungSu();
+  ks.name = '도경수';
+  print(ks.eat());
+  print(ks.say());
+  print(ks.gender());
+  // print(KyungSu().eat());
+  // print(KyungSu().say());
+
+} ////// testClass /////////////
+
+main(){
+  // 테스트 함수 호출
+  testClass();
+
+} /////////// main ////////////////
+
