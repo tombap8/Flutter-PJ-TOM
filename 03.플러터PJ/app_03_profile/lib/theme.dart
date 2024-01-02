92db88908dbdc81afbe2c0e7ed2cd0fc909e5ddb
Tom B. Parker <tombap8@hotmail.com>
2024-01-02 11:21:43 +0900

    ss

---
+++ 03.플러터PJ/app_03_profile/lib/theme.dart
@@ -0,0 +1,21 @@
+// 전체 앱의 테마색상 지정 클래스
+
+// 머티리얼 디자인 이므로 머티리얼컬러라고 명명
+// 공식 머티리얼 디자인 색상저정 클래스임!
+import 'package:flutter/material.dart';
+
+const MaterialColor primaryWhite = MaterialColor(
+    0xFFFFFFFF,
+    // 스와치는 여러색생을 맵형으로 지정함
+    <int, Color>{
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(0xFFFFFFFF),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
+    });
