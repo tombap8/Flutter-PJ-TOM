0f4d758f8fcc158a0732a75c7749dced4ea8bb85
Tom B. Parker <tombap8@hotmail.com>
2024-01-05 12:17:09 +0900

    Update profile_drawer.dart

--- 03.플러터PJ/app_03_profile/lib/components/profile_drawer.dart
+++ 03.플러터PJ/app_03_profile/lib/components/profile_drawer.dart
@@ -17,29 +17,115 @@ class ProfileDrawer extends StatelessWidget {
       // 박스 배경색
       color: Colors.blue,
       // 메뉴 넣기
-      child: Column(
-        children: [
-          SizedBox(height: 50),
-          // Home 메뉴 /////
-          Row(
-            children: [
-              // 아이콘
-              Icon(
-                Icons.home,
-                color: Colors.white,
-              ),
-              SizedBox(width: 5), // 간격
-              // 텍스트
-              Text(
-                "Home",
-                style: TextStyle(
-                  fontSize: 20,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            // 닫기버튼 /////
+            Row(
+              // 오른쪽 끝으로 정렬 : 수평방향이니까 Row에서는 mainAxis
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // 아이콘의 버튼화! -> InkWell() 위젯!
+                InkWell(
+                  // 탭이벤트시 드로어닫기!
+                  onTap: () {
+                    // debugPrint('닫어~!');
+                    // 이동클래스인 Navigator.pop() 메서드 사용
+                    Navigator.pop(context);
+                    // 내용으로 context만 쓰면 원래상태로 되돌림
+                    // 열었던 드로어가 닫힌다!
+                  },
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                ),
+                // 아이콘
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                // 아이콘
+                Icon(
+                  Icons.home,
                   color: Colors.white,
                 ),
-              ),
-            ],
-          ),
-        ],
+                SizedBox(width: 5), // 간격
+                // 텍스트
+                Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Store 메뉴 /////
+            Row(
+              children: [
+                // 아이콘
+                Icon(
+                  Icons.store,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 5), // 간격
+                // 텍스트
+                Text(
+                  "Store",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Community 메뉴 /////
+            Row(
+              children: [
+                // 아이콘
+                Icon(
+                  Icons.sensor_occupied_rounded,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 5), // 간격
+                // 텍스트
+                Text(
+                  "Community",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Development 메뉴 /////
+            Row(
+              children: [
+                // 아이콘
+                Icon(
+                  Icons.developer_mode,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 5), // 간격
+                // 텍스트
+                Text(
+                  "Development",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
