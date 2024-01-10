7a256d1db6992b585051e10ba725104f7c57c487
Tom B. Parker <tombap8@hotmail.com>
2024-01-10 12:58:56 +0900

    Update login_page.dart

--- 03.플러터PJ/app_04_login/lib/pages/login_page.dart
+++ 03.플러터PJ/app_04_login/lib/pages/login_page.dart
@@ -1,6 +1,8 @@
 // 로그인 페이지 /////
 
 import 'package:flutter/material.dart';
+// 로고위젯추가
+import 'package:app_04_login/components/logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,10 +14,22 @@ class LoginPage extends StatelessWidget {
       // 하단 입력키보드가 올라올때 대비할 수 있다!
       body: ListView(
         children: [
+          SizedBox(height: 20),
           // 로고보이기
+          Logo('Login'),
+          // 임시이동버튼
+          TextButton(
+            // 대상을 누를때 발생
+            onPressed: () {
+              // debugPrint('나눌렸어?');
+              // 라우터이동은 Navigator
+              // pushNamed() 메서드로!!!
+              Navigator.pushNamed(context, '/home');
+            },
+            child: Text('Go to Home!'),
+          )
         ],
       ),
-
     );
   }
 }
