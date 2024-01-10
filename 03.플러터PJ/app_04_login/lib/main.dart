7161a2f355fc04760449a31e491860e540bc5a75
Tom B. Parker <tombap8@hotmail.com>
2024-01-10 11:41:59 +0900

    ss

--- 03.플러터PJ/app_04_login/lib/main.dart
+++ 03.플러터PJ/app_04_login/lib/main.dart
@@ -6,11 +6,12 @@ void main() {
 
 /* 
   [ 로그인 앱 전체구성 : lib하위 ]
-  - 메인 출발(추가 사이즈지정 size.dart)
-  - 페이지구성 : 2개
+  1. 메인 출발(추가 사이즈지정 size.dart)
+    ->>> main.dart에 라우터 속성추가!!!
+  2. 페이지구성 : 2개
     pages / home_page.dart : 홈 페이지
     pages / login_page.dart : 로그인 페이지
-  - 컴포넌트 구성 : 3개
+  3. 컴포넌트 구성 : 3개
     components / logo.dart : SVG로고
     components / custom_form.dart : Form 위젯
     components / custom_text_form_field.dart : Form요소 위젯
@@ -23,7 +24,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,      
+      debugShowCheckedModeBanner: false,
+         
+      // 라우터를 사용할 경우 home속성을 사용하지 않는다!   
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
